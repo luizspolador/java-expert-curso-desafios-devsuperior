818b5be782910f213c167494d4279dd4f2a5b423
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-01-25 10:17:38 -0300

    desafio validação e segurança

---
+++ desafioParaEntrega-validacao-seguranca/src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    //RETORNAR TODAS AS CIDADES ORDENADAS POR NOME
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).toList();
+    }
+}
