2a6ea13519f685cf47ef93e39f361e31d57245f3
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-01-31 16:45:46 -0300

    finanlizando MovieControllerIT

--- desafioParaEntrega-movieflix-casos-de-uso/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ desafioParaEntrega-movieflix-casos-de-uso/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,14 +1,14 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.MovieCardDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -23,4 +23,14 @@ public class MovieController {
         MovieDetailsDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
+    @PreAuthorize("hasAnyRole('ROLE_VISITOR', 'ROLE_MEMBER')")
+    @GetMapping()
+    public ResponseEntity<Page<MovieCardDTO>> findByGenre(@RequestParam(required = false) Long genreId,
+                                                          @RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "10") int size) {
+        Page<MovieCardDTO> movies = service.findMoviesByGenre(genreId, PageRequest.of(page, size));
+        return ResponseEntity.ok().body(movies);
+    }
+
 }
