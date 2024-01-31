983941292e3f132a1316e5ecbd108d2cd7dcf385
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-01-31 15:38:03 -0300

    Finalizado testes GenreControllerIT

---
+++ desafioParaEntrega-movieflix-casos-de-uso/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @Transactional(readOnly = true)
+    public MovieDetailsDTO findById(Long id){
+        Optional<Movie> obj = repository.findById(id);
+        Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Filme inexistente"));
+        return new MovieDetailsDTO(entity);
+    }
+
+}
