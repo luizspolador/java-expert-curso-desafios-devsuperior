d4a7936bc017ce75eb03e15abccbfa850558501a
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2024-02-05 11:26:06 -0300

    MovieServiceTests

--- desafioParaEntrega-DSMovie-jacoco/src/test/java/com/devsuperior/dsmovie/tests/MovieFactory.java
+++ desafioParaEntrega-DSMovie-jacoco/src/test/java/com/devsuperior/dsmovie/tests/MovieFactory.java
@@ -9,6 +9,12 @@ public class MovieFactory {
 		MovieEntity movie = new MovieEntity(1L, "Test Movie", 0.0, 0, "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg");
 		return movie;
 	}
+
+	public static MovieEntity createMovieEntity(String title){
+		MovieEntity movie = createMovieEntity();
+		movie.setTitle(title);
+		return movie;
+	}
 	
 	public static MovieDTO createMovieDTO() {
 		MovieEntity movie = createMovieEntity();
